ce53cec7d9582f3d128fc5e23e12c82b62732e63
hyongok2 <hyongok2@naver.com>
2025-07-26 00:51:03 +0900

    인트로 실습

---
+++ intro/timecheck/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
+for (int i = 0; i < 1_000_000; i++)
+{
+    int x = i + 1229;
+}
+sw.Stop();
+Console.WriteLine($"Elapsed time: {sw.Elapsed.TotalMilliseconds} ms");
+Console.WriteLine($"Elapsed time: {sw.ElapsedTicks * (1_000_000.0 / Stopwatch.Frequency):F2}us");
